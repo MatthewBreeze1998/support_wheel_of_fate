bafc295d714fea4d83a0948df446ee49d4ee48fc
MatthewBreeze45 <60423656+MatthewBreeze45@users.noreply.github.com>
2022-07-13 11:04:24 +0100

    Update SupportWheelOfFate.dart

--- lib/Main/Routes/SupportWheelOfFate.dart
+++ lib/Main/Routes/SupportWheelOfFate.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-// ignore: import_of_legacy_library_into_null_safe
-import 'package:scoped_model/scoped_model.dart';
 import 'package:support_wheel_of_fate/Client.dart/EngineerClient.dart';
 import 'package:support_wheel_of_fate/DataModels/Engineer.dart';
 
